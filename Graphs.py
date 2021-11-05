5cf6a9a66165d8a9ce86c950f7868e9e3b25670c
Deadvitekchpool <vitek190499@gmail.com>
2021-11-05 19:36:06 +0300

    DFS fixes

--- Graphs.py
+++ Graphs.py
@@ -10,7 +10,31 @@ def depth_first(g, x, y):
 		print("X and Y vertices are not connected")
 	else:
 		i = dfs.index(y)
-		print("Minimal distance from " + str(x) + " to " + str(y) + ": " + str(len(dfs[:i])))
+		dfs = dfs[:i + 1]
+		length = len(dfs)
+		print("The road of the search:")
+		print(dfs)
+		adj_list = g.get_adjlist()
+		dfs_copy = dfs
+		i = 0
+		while True:
+			init_point = dfs_copy[i]
+			if init_point == dfs_copy[-1]:
+				break
+			else:
+				temp = adj_list[init_point]
+				k = 0
+				for e in reversed(dfs_copy[dfs_copy.index(init_point) + 1:]):
+					if e in temp:
+						k = dfs_copy.index(e)
+						break
+				if k:
+					dfs_copy = dfs_copy[:i + 1] + dfs_copy[k:]
+					print(dfs_copy, dfs_copy[i])
+				i += 1
+
+		# print(dfs_copy)
+		# print("Minimal distance from " + str(x) + " to " + str(y) + ": " + str(length))
 
 def breadth_first(g, x, y):
 	bfs = g.bfs(x)[0]
@@ -18,6 +42,20 @@ def breadth_first(g, x, y):
 		print("X and Y vertices are not connected")
 	else:
 		i = bfs.index(y)
+		bfs = bfs[:i + 1]
+		length = len(bfs)
+		print("The road of the search:")
+		print(bfs)
+		adj_list = g.get_adjlist()
+		for i in range(0, len(bfs) - 1):
+			init_point = bfs[i]
+			temp = adj_list[init_point]
+			k = 0
+			for p in temp:
+				if p in bfs[i + 1:]:
+					k += 1
+			length = length - k + 1		
+
 		print("Minimal distance from " + str(x) + " to " + str(y) + ": " + str(len(bfs[:i])))
 
 graph = erdos_renyi_graph(100, 0.05)
@@ -76,13 +114,15 @@ temp = g.get_adjlist()
 for i in range(100):
 	adj_list[i] = temp[i]
 
-for i in range(10):
+for i in range(100):
 	print(str(i) + ": " + str(adj_list[i]))
 
-print(g.get_adjacency()[:10])
+# print(g.get_adjacency()[:10])
 
-breadth_first(g, 0, 1)
 depth_first(g, 0, 1)
+# print(dfs(temp, 0, 1))
+# breadth_first(g, 0, 1)
+
 fig, ax = plt.subplots()
 plot(g, layout=layout, bbox=(300, 300), margin=20, target=ax)
-plt.show()
+# plt.show()
